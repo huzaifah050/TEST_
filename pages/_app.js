90337bad62226d656a68edae223f5b6f9d84a43a
Huzaifah Yusif-Attah <hhuzaifah050@gmail.com>
2022-05-01 12:58:31 +0000

    Clean up and setting Chakra UI

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react';
+import { Provider } from 'react-redux';
+import { store } from '../src/store';
+import theme from '../src/theme';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
+
+let persistor = persistStore(store);
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<Provider store={store}>
+			<PersistGate loading={null} persistor={persistor}>
+				<ChakraProvider theme={theme}>
+					<Component {...pageProps} />
+				</ChakraProvider>
+			</PersistGate>
+		</Provider>
+	);
 }
 
-export default MyApp
+export default MyApp;
